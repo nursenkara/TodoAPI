03bbeed7b2de4ccbf698eef96f1b2027d0dd9687
Nur Şenkara <nsenkara19@gmail.com>
2022-03-21 12:07:45 +0300

    Repository class oluşturuldu. User ve Todo repositoryler eklendi.

---
+++ Todo.Data/Repositories/TodoRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Core.Models;
+using Todo.Core.Repositories;
+
+namespace Todo.Data.Repositories
+{
+    public class TodoRepository : Repository<Todos>, ITodoRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public TodoRepository(AppDbContext context):base(context)
+        {
+
+        }
+        public async Task<Todos> GetWithUserByIdAsync(int todoId)
+        {
+            return await _appDbContext.Todos.Include(x => x.User).SingleOrDefaultAsync(x => x.Id == todoId);
+        }
+    }
+}
---
+++ Todo.Data/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Core.Models;
+using Todo.Core.Repositories;
+
+namespace Todo.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public UserRepository(AppDbContext context): base(context)
+        {
+
+        }
+        public async Task<User> GetWithTodoByIdAsync(int userId)
+        {
+            return await _appDbContext.Users.Include(x => x.Todos).SingleOrDefaultAsync(x => x.Id == userId);
+        }
+    }
+}
